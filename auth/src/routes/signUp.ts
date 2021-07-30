449c0851e47f6c5ebd413449bc965db749103851
sscarry2 <earth.narongsak@gmail.com>
2021-07-30 23:53:24 +0700

    add middlewares errorHandler

--- auth/src/routes/signUp.ts
+++ auth/src/routes/signUp.ts
@@ -16,14 +16,15 @@ router.post('/api/users/signup',
 (req: Request, res: Response) => {
     const errors = validationResult(req)
 
-    if(!errors.isEmpty()){
-        return res.status(400).send(errors.array())
+    if(!errors.isEmpty()){        
+        throw new Error('Invalid email or password')
     }
 
-    const { email, password } = req.body
+    // const { email, password } = req.body
     
     console.log('Creating a user...');
-    
+    throw new Error('Error connecting to database')
+
    res.send({})
 })
 export { router as signUpRouter }
