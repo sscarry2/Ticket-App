a53636699c071750ccdaef5fb2d4c414b09f6065
sscarry2 <earth.narongsak@gmail.com>
2021-08-07 01:34:06 +0700

    signup form

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post(
+      "https://ticketing.dev/api/users/signup",
+      {
+        email,
+        password,
+      }
+    );
+
+    console.log(response);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          className="form-control"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUp;
