6f741fb22f82d4546590064ae21821de90f98993
sscarry2 <earth.narongsak@gmail.com>
2021-08-08 01:04:27 +0700

    call service

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,30 @@
-const HelloWorld = () => {
-  return <h1>HelloWorld</h1>;
+import axios from "axios";
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
+
+  return <h1>Landing Page</h1>;
 };
 
-export default HelloWorld;
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  if (typeof window === "undefined") {
+    //we are on the server
+    const { data } = await axios.get(
+      "http://ingress-nginx.ingress-nginx-controller.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    //we are on the browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
