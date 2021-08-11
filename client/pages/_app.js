74cc3aea1a6c772e796ef141a064290a83a70c6b
sscarry2 <earth.narongsak@gmail.com>
2021-08-11 23:45:33 +0700

    sign in form & custom app getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,25 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header !</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default App;
+AppComponent.getInitialProps = (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return data;
+};
+
+export default AppComponent;
